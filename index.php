26af1195dc6a9102e60c51b04cd0bb10b56e60cd
kopmean <73383077+kopmean@users.noreply.github.com>
2020-10-30 17:20:14 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Hello world</title>
+    </head>
+    <body>
+        <h1>My First Heading</h1>
+        <p>My First Paragraph</p>
+        <img src="file-20200803-24-50u91u.jpg">
+        <br>
+        <a href="http://www.it.kmitl.ac.th">This is a Link</a>
+        <h2>The unordered list</h2>
+        <ul>
+            <li>Google</li>
+        </ul>
+    </body>
+</html>
