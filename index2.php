fccb40b13b6b934e17a25a642a9661ad29959e46
kopmean <73383077+kopmean@users.noreply.github.com>
2020-11-06 18:37:44 +0700

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+</head>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6">
+            <img src="img/work/work1.jpg" style="width: 100%; height:100%;" class="img-fluid" />
+        </div>
+        <div class="col-sm-3">
+            <div class="col">
+                <img src="banner.jpg" style="width: 100%; height:33%;" class="img-fluid" />
+                <img src="office1.jpg" style="width: 100%; height:33%;" class="img-fluid" />
+                <img src="creative3.jpg" style="width: 100%; height:33%;" class="img-fluid" />
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <img src="life.jpg" style="width: 100%;height:66%;" class="img-fluid" />
+            <img src="work8.jpg" style="height:32%; width: 100%;" class="img-fluid" />
+        </div>
+    </div>
