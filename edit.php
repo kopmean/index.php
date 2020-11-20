b92c588e5b71c89233487c739d698c277aaf8e24
kopmean <73383077+kopmean@users.noreply.github.com>
2020-11-20 15:34:08 +0700

    Add files via upload

---
+++ edit.php
@@ -0,0 +1,27 @@
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'itf63.mysql.database.azure.com', 'kopsira@itf63', 'GQFvju56', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$ID = $_GET['ID']
+$row = mysqli_query($conn ,"Select * From guestbook where ID = $ID")
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Comment Form</title>
+</head>
+<body>
+  <form action = "" method = "post" id="CommentForm" >
+    Name:<br>
+    <input type="text" name = "name" id="idName" value="<?php = $row["Name"]; ?>"> <br>
+    Comment:<br>
+    <textarea rows="10" cols="20" name = "comment" id="idComment" value="<?php = $row["Comment"]; ?>"></textarea><br>  
+    Link:<br>
+    <input type="text" name = "link" id="idLink" value="<?php = $row["Link"]; ?>"> <br><br>
+    <input type="submit" id="commentBtn">
+  </form> 
+</body>
+</html>
