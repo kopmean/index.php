691136dc1c306dd5854d4ac78612ea303359d85e
kopmean <73383077+kopmean@users.noreply.github.com>
2020-11-26 16:02:12 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -17,7 +17,7 @@ $sql = "INSERT INTO guestbook (name , comment , link) VALUES ('$name', '$comment
 
 
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
+    header("location:show.php");
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
