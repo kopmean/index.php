f17ba1b501eb55bde2a74ed4fd1e2593d073d13e
kopmean <73383077+kopmean@users.noreply.github.com>
2020-11-27 17:03:45 +0700

    Create database.php

---
+++ database.php
@@ -0,0 +1,27 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'itf63.mysql.database.azure.com', 'kopsira@itf63', 'GQFvju56', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$name = $_GET['name'];
+$weight = $_GET['weight'];
+$height = $_GET['height'];
+$bmi = weight/height
+
+
+$sql = "INSERT INTO bmi (name , weight , height , bmi) VALUES ('$name', '$weight', '$height', '$bmi')";
+
+
+if (mysqli_query($conn, $sql)) {
+    header("location:show.php");
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
